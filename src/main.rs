f404d20fd126b8d13921edd5bea83e3c55d15dd2
nozomemein <b8yukifsukeo999n@gmail.com>
2025-07-04 16:27:31 +0900

    add configuration.yaml

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::net::TcpListener;
 
-use zero2prd_rs::run;
+use zero2prd_rs::{configuration::get_configuration, run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind port 8080");
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
