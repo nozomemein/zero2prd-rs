95768696105b63c6369569a34cf9d7ddcb7cc00c
nozomemein <b8yukifsukeo999n@gmail.com>
2025-07-21 15:56:45 +0900

    refactor with tracing macros

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,6 @@
 use actix_web::{HttpResponse, web};
 use chrono::Utc;
 use sqlx::PgPool;
-use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -12,31 +11,46 @@ pub struct FormData {
 
 #[tracing::instrument(
   name = "Adding a new subscriber",
-   skip(form, pool),
-    fields(request_id = %Uuid::new_v4(),
+  skip(form, pool),
+  fields(
+    request_id = %Uuid::new_v4(),
     subscriber_email = %form.email,
-    subscriber_name = %form.name)
+    subscriber_name = %form.name
+  )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
-    match sqlx::query!(
-        r#"
-      INSERT INTO subscriptions (id, email, name, subscribed_at)
-      VALUES ($1, $2, $3, $4)
-      "#,
-        Uuid::new_v4(),
-        form.email,
-        form.name,
-        Utc::now(),
-    )
-    .execute(pool.get_ref())
-    .instrument(query_span)
-    .await
-    {
+    match insert_subscriber(form, pool).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
             tracing::error!("Failed to execute query: {e:?}");
             HttpResponse::InternalServerError().finish()
         }
     }
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+    INSERT INTO subscriptions (id, email, name, subscribed_at)
+    VALUES ($1, $2, $3, $4)
+    "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now(),
+    )
+    .execute(pool.get_ref())
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {e:?}");
+        e
+    })?;
+    Ok(())
 }
